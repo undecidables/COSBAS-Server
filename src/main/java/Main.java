da18e4b2b8329f82c8636bac129508cb8e048beb
Renette Ros <renetteros@gmail.com>
2015-05-28 22:44:56 +0200

    Basic gradle project structure with a build file most of the planned dependencies.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Renette on 2015-05-28.
+ * A Hello world class to test the gradle project setup.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
