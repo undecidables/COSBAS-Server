98f3705c50897318ae650f9c4d26ee9052cb8a46
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-06-27 12:11:15 +0200

    Request and Cancel Appointment skeletons

---
+++ src/main/java/Appointment/Appointments.java
@@ -0,0 +1,23 @@
+package Appointment;
+
+import java.util.Date;
+
+/**
+ * Appointments class used to Request and cancel appointments. 
+ * @author Elzahn
+ */
+public class Appointments 
+{
+    
+    public String requestAppointment(String studentID, String lecturerID, Date dateTime)
+    {
+        String appointmentID = "";
+        
+        return appointmentID;
+    }
+    
+    public void cancelAppointment(String cancelleeID, String appointmentID)
+    {
+        
+    }
+}
