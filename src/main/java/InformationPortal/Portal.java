40420f9df8fc7a72c57f2f143f53dc4f324ab425
Szymon Ziolkowski <nomyzs.z@gmail.com>
2015-07-01 20:30:44 +0200

    Added a bit of structure to information portal

---
+++ src/main/java/InformationPortal/Portal.java
@@ -0,0 +1,38 @@
+package InformationPortal;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by Szymon on 01/07/2015.
+ */
+public class Portal {
+
+    /**
+     * Can't think of the parameters to pass in or return results at this moment
+     */
+
+    public void addPost()
+    {
+
+    }
+
+    public void deletePost()
+    {
+
+    }
+
+    public ArrayList<Post> getPosts()
+    {
+        ArrayList<Post> posts = new ArrayList<Post>();
+
+
+        return posts;
+    }
+
+    public void editPost()
+    {
+
+    }
+
+}
