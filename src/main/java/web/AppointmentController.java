035df9fac8a07a8dcedee236a9cf2613d7924e2f
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-07-15 15:58:07 +0200

    Altered webpages' structure to be more visual

--- src/main/java/web/AppointmentController.java
+++ src/main/java/web/AppointmentController.java
@@ -11,93 +11,110 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.security.Principal;
 
 @Controller
 public class AppointmentController {
 
   /**
-   * An Example route, can be removed once we have something definite
-   * @param name
-   * @return greeting page
-   */
-  @RequestMapping(method= RequestMethod.GET, value="/greeting")
-  public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
-    model.addAttribute("name", name);
-    return "greeting";
-  }
-
-  /**
-  * Route function to go to index.html
-  * @param name - used to determin which menu to use. If anything is stored in name the logged in menu is used. 
+  * Route function to go to index.html - Homepage for the user
   * @return index.html page
   */
   @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String index(@RequestParam(value = "name", required = false, defaultValue = "") String name, Model model)
+  public String index(Principal principal)
   {
-    model.addAttribute("name", name);
-    return "index";
+    return principal == null ? "makeAppointment" : "index";
+    //return "index";
   }
 
   /**
-  * Route function to go to login.html
-  * @param name - used to determin which menu to use. If anything is stored in name the logged in menu is used. 
+  * Route function to go to login.html - Login page for staffmembers to login on
   * @return login.html page
   */
   @RequestMapping(value = "/login", method = RequestMethod.GET)
-  public String login(@RequestParam(value = "name", required = false, defaultValue = "") String name, Model model)
+  public String login()
   {
-    model.addAttribute("name", name);
     return "login";
   }
 
   /**
-  * Route function to go to makeAppointment.html
-  * @param name - used to determin which menu to use. If anything is stored in name the logged in menu is used. 
+  * Route function to go to makeAppointment.html - Page for users to request an appointment with a staffmember
   * @return makeAppointment.html page
   */
   @RequestMapping(value = "/makeAppointment", method = RequestMethod.GET)
-  public String makeAppointment(@RequestParam(value = "name", required = false, defaultValue = "") String name, Model model)
+  public String makeAppointment()
   {
-    model.addAttribute("name", name);
     return "makeAppointment";
   }
 
   /**
-  * Route function to go to logout.html
-  * @param name - used to determin which menu to use. If anything is stored in name the logged in menu is used. 
+  * Route function to go to logout.html - confirmation page to ensure user wants to logout
   * @return logout.html page
   */
   @RequestMapping(value = "/logout", method = RequestMethod.GET)
-  public String logout(@RequestParam(value = "name", required = false, defaultValue = "") String name, Model model)
+  public String logout()
   {
-    model.addAttribute("name", name);
     return "logout";
   }
 
   /**
-  * Route function to go to checkAppointment.html
-  * @param name - used to determin which menu to use. If anything is stored in name the logged in menu is used. 
+  * Route function to go to checkAppointment.html - Page where staff members can check all their appointments
   * @return checkAppointment.html page
   */
   @RequestMapping(value = "/checkAppointment", method = RequestMethod.GET)
-  public String checkAppointment(@RequestParam(value = "name", required = false, defaultValue = "") String name, Model model)
+  public String checkAppointment()
   {
-    model.addAttribute("name", name);
     return "checkAppointment";
   }
 
   /**
-  * Route function to go to addUser.html
-  * @param name - used to determin which menu to use. If anything is stored in name the logged in menu is used. 
+  * Route function to go to addUser.html - Page where a new user can be added to the system
   * @return addUser.html page
   */
   @RequestMapping(value = "/addUser", method = RequestMethod.GET)
-  public String addUser(@RequestParam(value = "name", required = false, defaultValue = "") String name, Model model)
+  public String addUser()
   {
-    model.addAttribute("name", name);
     return "addUser";
   }
 
-  //TODO: Map /  (root)
-  //TODO: Map /error
+ /**
+  * Route function to go to status.html - Page where a user can check the status of their appointments
+  * @return status.html page
+  */
+  @RequestMapping(value = "/status", method = RequestMethod.GET)
+  public String status()
+  {
+    return "status";
+  }
+
+   /**
+  * Route function to go to cancel.html - Page where a user can cancel their appointments
+  * @return cancel.html page
+  */
+  @RequestMapping(value = "/cancel", method = RequestMethod.GET)
+  public String cancel()
+  {
+    return "cancel";
+  }
+
+ /**
+  * Route function to go to approveDenyAppointment.html - Page where a staff member can approve or deny requested appointments
+  * @return approveDenyAppointment.html page
+  */
+  @RequestMapping(value = "/approveDenyAppointment", method = RequestMethod.GET)
+  public String approveDenyAppointment()
+  {
+    return "approveDenyAppointment";
+  }
+
+
+ /**
+  * Route function to go to adderror.html - Page showing the user what error occured
+  * @return error.html page
+  */
+  @RequestMapping(value = "/error", method = RequestMethod.GET)
+  public String error()
+  {
+    return "error";
+  }
 }
