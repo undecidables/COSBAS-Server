a470c55ae386adbecebd54e288c763fa89b7dcba
Szymon Ziolkowski <nomyzs.z@gmail.com>
2015-07-15 10:44:36 +0200

    Revert "Made some changes to websecurity, removed CSFR protection on /biometrics/access"
    This reverts commit 02c538853f7b53ff969a6d2105e88fd5412e9eb3.

--- src/main/java/web/WebSecurityConfig.java
+++ src/main/java/web/WebSecurityConfig.java
@@ -13,11 +13,6 @@ import org.springframework.security.config.annotation.authentication.configurers
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.regex.Pattern;
 
 /**
  * This class is a Spring Security configuration file. This class is responsible for authenticating a user via LDAP and automatically integrates with the Spring MVC.
@@ -33,40 +28,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        // Build the request matcher for CSFR protection
-        RequestMatcher csrfRequestMatcher = new RequestMatcher() {
-            // Disable CSFR protection on the following urls:
-            private AntPathRequestMatcher[] requestMatchers = {
-                    new AntPathRequestMatcher("/biometrics/access")
-            };
-
-            @Override
-            public boolean matches(HttpServletRequest request) {
-                // If the request match one url the CSFR protection will be disabled
-                for (AntPathRequestMatcher rm : requestMatchers) {
-                    if (!rm.matches(request)) {
-                        continue;
-                    }
-                    return false;
-                }
-                return true;
-            }
-        };
-
         http
-                .csrf().requireCsrfProtectionMatcher(csrfRequestMatcher)
-                .and()
                 .authorizeRequests()
                 .antMatchers("/resources/**").permitAll() //Allows people to access our resources
                 .antMatchers("/nav/*").permitAll() //This allows us to navigate all pages without having to login. This may need some customization.
                 .antMatchers("/login").permitAll() //This allows us to navigate all pages without having to login. This may need some customization.
                 .antMatchers("/").permitAll() //This allows us to navigate all pages without having to login. This may need some customization.
-                .antMatchers("/biometrics/access").permitAll() //Should we have login for pi-clients?
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
+
                 .permitAll();
     }
 
