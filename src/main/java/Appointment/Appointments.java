0aae98be593eba78bc62cb406c74b6fcb4759d24
Renette Ros <renetteros@gmail.com>
2015-07-21 00:08:06 +0200

    Dependency injection for database repository into appointments class

--- src/main/java/Appointment/Appointments.java
+++ src/main/java/Appointment/Appointments.java
@@ -1,13 +1,31 @@
 package appointment;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import web.mongo_repositories.AppointmentRepository;
+
 import java.time.LocalDateTime;
 
 /**
  * Appointments class used to Request and cancel appointments. 
  * @author Elzahn
  */
+
 public class Appointments 
 {
+
+    /**
+     * The database adapter/repository to use.
+     */
+    @Autowired
+    private AppointmentDBAdapter repository;
+
+    /**
+     * Setter based dependency injection since mongo automatically creates the bean.
+     * @param repository The repository to be injected.
+     */
+    public void setRepository(AppointmentDBAdapter repository) {
+        this.repository = repository;
+    }
     
     public String requestAppointment(String appointerID, String staffID, LocalDateTime dateTime, String reason, int durationInMinutes)
     {
