acf941f4f097e399ff6c512288cc82ec4495e5ce
Vivian <u13238435@tuks.co.za>
2015-07-23 21:53:05 +0200

    Added the PersonRepo and AccessRepo to the BiometricSystem class to store the details on the db when entering a building. I do however not know if it is in the correct place as of yet.

--- src/main/java/biometric/BiometricSystem.java
+++ src/main/java/biometric/BiometricSystem.java
@@ -1,10 +1,37 @@
 package biometric;
 
+import appointment.AppointmentDBAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * @author Renette
  * Modules handles core biometric functions.
  */
 public class BiometricSystem {
+
+    /**
+     * The database adapter/repository to use.
+     */
+    @Autowired
+    private AccessDBAdapter AccessRepository;
+
+    @Autowired
+    private PersonDBAdapter PersonRepository;
+
+
+    /**
+     * Setter based dependency injection since mongo automatically creates the bean.
+     * @param repository The repository to be injected.
+     */
+    public void setRepository(AccessDBAdapter repository) {
+        this.AccessRepository = repository;
+    }
+
+    public void setRepository(PersonDBAdapter repository) {
+        this.PersonRepository = repository;
+    }
+
+
    public AccessResponse requestAccess(AccessRequest req) {
         //Create correct type of access validator from request
         //Read from config file or something?
