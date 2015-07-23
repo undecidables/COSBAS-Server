01137ccedadd020c1fe89603f1dc4bfdf08a18cd
Renette Ros <renetteros@gmail.com>
2015-07-23 21:17:27 +0200

    Removed a bit of redudancy from previous commit

--- src/main/java/Appointment/AppointmentDBAdapter.java
+++ src/main/java/Appointment/AppointmentDBAdapter.java
@@ -10,7 +10,7 @@ import java.util.List;
  *  This interface exists for the sake of making the type of database pluggable.
  *  Extra Functions follow the query style of MongoRepositories.
  */
-public interface AppointmentDBAdapter extends CrudRepository<appointment.Appointment, String> {
+public interface AppointmentDBAdapter extends CrudRepository<Appointment, String> {
 
     List<Appointment> findByStaffID(String staffID);
     Appointment findById(String id);
