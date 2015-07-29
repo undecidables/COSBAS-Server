e6e15450064c7b62a0c91616ba8ed153e09ac6f5
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-07-29 13:42:56 +0200

    Unit test skeletons

--- src/test/java/AppointmentTests.java
+++ src/test/java/AppointmentTests.java
@@ -2,13 +2,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import junit.framework.TestCase;
 import org.junit.Before;
@@ -16,7 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import authentication.Authenticator;
+import appointments.Appointments;
 
 /**
  * This class is the unit tested for the authentication package. It contains unit tests to make sure successful/unsuccessful authentication happens at the correct places.
@@ -29,13 +22,65 @@ public class AppointmentTests {
 
     private final Appointments appointments;
 
+    /**
+     * This method is used to select the calander to be used as well as the available times for appointments
+     */
+    @Before
+    public void setUpCalendarAvaliabiltiy() {
+        
+    }
+
     /*
-    //Tests to do
+    Tests to do
     requestAppointment
         one username
         multiple users
+        multiple users requesting same time
         staff member
+    */
 
+    /**
+     * This method tests a single user requesting an appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testOneUserAppointment() throws IOException
+    {
+       
+    }
+
+     /**
+     * This method tests multiple user requesting an appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testMultipleUsersAppointment() throws IOException
+    {
+       
+    }
+
+     /**
+     * This method tests multiple user requesting the same appointment time
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testMultipleUsersSameTimeAppointment() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests a staff member requesting an Appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testStaffMemberAppointment() throws IOException
+    {
+       
+    }
+    
+    /*
+    Tests to do
     cancelAppointment
         single member wrong appointments
         single member wrong person
@@ -44,7 +89,80 @@ public class AppointmentTests {
         staff member right appointment 
         staff member wrong person
         already cancelled appointment
+    */
 
+    /**
+     * This method tests cancelling an non-existing appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testCancellingNonExistingAppointment() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests a unauthorized user cancelling an appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testUnaurtherizedUserCancelling() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests cancelling an appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testUserCancelling() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests a user part of a group cancelling an appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testUserInAListCancelling() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests an unauthorized staff member cancelling an Appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testUnauthorizedStaffMemberCancelling() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests a staff member cancelling an Appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testStaffMemberCancelling() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests cancelling an appointment that was already cancelled
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testCancellingAlreadyCancelledAppointment() throws IOException
+    {
+       
+    }
+
+    /*
+    Tests to do
     checkStatus
         single member wrong appointments
         single member wrong person
@@ -52,140 +170,164 @@ public class AppointmentTests {
         other in the list member right person
         staff member right appointment 
         staff member wrong person
+    */
 
+    /**
+     * This method tests checking an appointment's status for a non-existing appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testCheckStatusNonExistingAppointment() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests checking an appointment's status by an unauthorized user
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testCheckStatusUnauthorizedUser() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests checking an appointment's status
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testCheckStatus() throws IOException
+    {
+       
+    }
+
+    /**
+     *  This method tests checking an appointment's status by a user part of a group
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testCheckStatusUserInAList() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests checking an appointment's status by an unauthorized staff member
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testCheckStatusStaffMemberUnauthorizedUser() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests checking an appointment's status by an authorized staff member
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testCheckStatusStaffMember() throws IOException
+    {
+       
+    }
+
+    /*
+    Tests to do
     approveAppointment
         wrong appointment 
         correct appointment not requested
         correct appointment
+    */
 
+    /**
+     * This method tests the approval of an appointment by an unathorized user
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testApproveAppointmentUnauthorizedUser() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests the approval of an appointment that's status isn't "requested"
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testApproveAppointmentAppointmentNotRequested() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests the approval of an appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testApproveAppointment() throws IOException
+    {
+       
+    }
+
+    /**
+     * This method tests the approval of an non-existing appointment
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testApproveAppointmentNonExisting() throws IOException
+    {
+       
+    }
+
+    /*
+    Tests to do
     denyAppointment
         wrong appointment 
         correct appointment not requested
         correct appointment
-
     */
-    /*
 
     /**
-     * This method sets up the value to be returned if getSession is called - essentially it provides a value for the Mock object.
-     *
-    @Before
-    public void setUp() {
-        when(request.getSession()).thenReturn(session);
+     * This method tests the denial of an appointment by an unathorized user
+     *              what does it do and return and when sucessfull?
+     */
+    @Test
+    public void testDenyAppointmentUnauthorizedUser() throws IOException
+    {
+       
     }
 
     /**
-     * This method is used to test a bad password against a username.
-     * Does this by setting up values which should be returned by the request object's params
-     * Then creating an authenticator object and doing the authentication against LDAP and returning an HttpSession
-     * Should return "authentication failed" - the unit test will fail otherwise.
-     *
+     * This method tests the denial of an appointment that that's status isn't "requested"
+     *              what does it do and return and when sucessfull?
+     */
     @Test
-    public void testBadStudentPassword() throws IOException, ServletException
+    public void testDenyAppointmentAppointmentNotRequested() throws IOException
     {
-        HttpServletRequest stubHttpServletRequest = mock(HttpServletRequest.class);
-        HttpServletResponse stubHttpServletResponse = mock(HttpServletResponse.class);
-        HttpSession stubHttpSession = mock(HttpSession.class);
-
-        when(stubHttpServletRequest.getParameter("username")).thenReturn("u89000121");
-        when(stubHttpServletRequest.getParameter("password")).thenReturn("caloooo");
-
-        when(stubHttpServletRequest.getSession()).thenReturn(stubHttpSession);
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw  =new PrintWriter(sw);
-
-        when(stubHttpServletResponse.getWriter()).thenReturn(pw);
-
-        Authenticator sampleServlet =new Authenticator();
-        sampleServlet.doProcess(stubHttpServletRequest, stubHttpServletResponse);
-        String result = sw.getBuffer().toString().trim();
-        TestCase.assertEquals(result, new String("Authentication Failed"));
-
-
+       
     }
+
     /**
-     * This method is used to test a good password against a username.
-     * Does this by setting up values which should be returned by the request object's params
-     * Then creating an authenticator object and doing the authentication against LDAP and returning an HttpSession
-     * Should return "username + password + authenticated" - the unit test will fail otherwise.
-     *
+     * This method tests the denial of an appointment
+     *              what does it do and return and when sucessfull?
+     */
     @Test
-    public void testGoodStudentPassword() throws IOException, ServletException
+    public void testDenyAppointment() throws IOException
     {
-        HttpServletRequest stubHttpServletRequest = mock(HttpServletRequest.class);
-        HttpServletResponse stubHttpServletResponse = mock(HttpServletResponse.class);
-        HttpSession stubHttpSession = mock(HttpSession.class);
-
-        when(stubHttpServletRequest.getParameter("username")).thenReturn("u89000121");
-        when(stubHttpServletRequest.getParameter("password")).thenReturn("Green");
-
-        when(stubHttpServletRequest.getSession()).thenReturn(stubHttpSession);
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw  =new PrintWriter(sw);
-
-        when(stubHttpServletResponse.getWriter()).thenReturn(pw);
-
-        Authenticator sampleServlet =new Authenticator();
-        sampleServlet.doProcess(stubHttpServletRequest, stubHttpServletResponse);
-        String result = sw.getBuffer().toString().trim();
-        TestCase.assertEquals(result, new String("u89000121 Green authenticated"));
+       
     }
+
     /**
-     * This method is used to test a good password against a staff username.
-     * Does this by setting up values which should be returned by the request object's params
-     * Then creating an authenticator object and doing the authentication against LDAP and returning an HttpSession
-     * Should return "username + password + authenticated" - the unit test will fail otherwise.
-     *
+     * This method tests the denial of an non-existing appointment
+     *              what does it do and return and when sucessfull?
+     */
     @Test
-    public void testGoodStaffPassword() throws IOException, ServletException
+    public void testDenyAppointmentNonExisting() throws IOException
     {
-        HttpServletRequest stubHttpServletRequest = mock(HttpServletRequest.class);
-        HttpServletResponse stubHttpServletResponse = mock(HttpServletResponse.class);
-        HttpSession stubHttpSession = mock(HttpSession.class);
-
-        when(stubHttpServletRequest.getParameter("username")).thenReturn("BCrawley");
-        when(stubHttpServletRequest.getParameter("password")).thenReturn("Crawley");
-
-        when(stubHttpServletRequest.getSession()).thenReturn(stubHttpSession);
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw  =new PrintWriter(sw);
-
-        when(stubHttpServletResponse.getWriter()).thenReturn(pw);
-
-        Authenticator sampleServlet =new Authenticator();
-        sampleServlet.doProcess(stubHttpServletRequest, stubHttpServletResponse);
-        String result = sw.getBuffer().toString().trim();
-        TestCase.assertEquals(result, new String("BCrawley Crawley authenticated"));
+       
     }
-    /**
-     * This method is used to test a bad password against a staff username.
-     * Does this by setting up values which should be returned by the request object's params
-     * Then creating an authenticator object and doing the authentication against LDAP and returning an HttpSession
-     * Should return "username + password + authenticated" - the unit test will fail otherwise.
-     *
-    @Test
-    public void testBadStaffPassword() throws IOException, ServletException
-    {
-        HttpServletRequest stubHttpServletRequest = mock(HttpServletRequest.class);
-        HttpServletResponse stubHttpServletResponse = mock(HttpServletResponse.class);
-        HttpSession stubHttpSession = mock(HttpSession.class);
 
-        when(stubHttpServletRequest.getParameter("username")).thenReturn("BCrawley");
-        when(stubHttpServletRequest.getParameter("password")).thenReturn("Green");
-
-        when(stubHttpServletRequest.getSession()).thenReturn(stubHttpSession);
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw  =new PrintWriter(sw);
-
-        when(stubHttpServletResponse.getWriter()).thenReturn(pw);
-
-        Authenticator sampleServlet =new Authenticator();
-        sampleServlet.doProcess(stubHttpServletRequest, stubHttpServletResponse);
-        String result = sw.getBuffer().toString().trim();
-        TestCase.assertEquals(result, new String("Authentication Failed"));
-    }*/
+    //Add aditional tests for calendar and availability testing
+    
 }
