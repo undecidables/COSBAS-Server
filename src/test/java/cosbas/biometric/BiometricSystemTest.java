c5f12922f9095efe675c8ada20ad6cff6c134ed3
Renette Ros <renetteros@gmail.com>
2015-08-19 18:51:38 +0200

    Generated tests for BiometricSystem

---
+++ src/test/java/cosbas/biometric/BiometricSystemTest.java
@@ -0,0 +1,37 @@
+package cosbas.biometric;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Renette
+ */
+public class BiometricSystemTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testRequestAccess() throws Exception {
+
+    }
+
+    @Test
+    public void testAddUser() throws Exception {
+
+    }
+
+    @Test
+    public void testApproveUser() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveUser() throws Exception {
+
+    }
+}
