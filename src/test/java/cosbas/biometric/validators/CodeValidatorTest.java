2288aa6aa7930b1396b11697a3a6654ac0ce50b5
Renette Ros <renetteros@gmail.com>
2015-08-19 08:37:01 +0200

    Access Code Unit Testing

--- src/test/java/cosbas/biometric/validators/CodeValidatorTest.java
+++ src/test/java/cosbas/biometric/validators/CodeValidatorTest.java
@@ -1,29 +1,17 @@
 package cosbas.biometric.validators;
 
 import cosbas.biometric.data.AccessCode;
-import cosbas.biometric.data.BiometricData;
 import cosbas.biometric.data.BiometricDataDAO;
-import cosbas.biometric.validators.exceptions.AccessCodeException;
 import cosbas.biometric.validators.exceptions.ValidationException;
-import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.temporal.TemporalUnit;
-import java.util.ArrayList;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
-import static junit.framework.TestCase.assertFalse;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Renette
@@ -34,15 +22,14 @@ public class CodeValidatorTest {
     BiometricDataDAO repository;
     CodeValidator testee;
 
-    private static byte[] code1 = new byte[] {1,2,3,4,5};
-    private static byte[] code2 = new byte[] {4,5,6,7,8};
-    private static byte[] code3 = new byte[] {5,6,6,7,8};
-    private static byte[] code4 = new byte[] {7,5,6,7,8};
-    private LocalDateTime now;
+    private static final byte[] code1 = new byte[] {1,2,3,4,5};
+    private static final byte[] code2 = new byte[] {4,5,6,7,8};
     private LocalDateTime before1;
     private LocalDateTime before2;
     private LocalDateTime after1;
     private LocalDateTime after2;
+    private AccessCode req1 = new AccessCode(code1);;
+    private AccessCode req2 = new AccessCode(code2);;
 
     @Before
     public void setUp() throws Exception {
@@ -50,40 +37,54 @@ public class CodeValidatorTest {
         testee = spy(CodeValidator.class);
         testee.setRepository(repository);
 
-        now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now();
 
         before1 = now.minusHours(1);
         before2 = now.minusHours(2);
         after1 = now.plusHours(1);
         after2 = now.plusHours(2);
+
     }
 
     @Test
     public void testMatches() throws Exception {
+        AccessCode valid = new AccessCode("user", code1);
 
-
-        AccessCode valid1 = new AccessCode("u0000000", code1);
-        AccessCode req1 = new AccessCode(code1);
-        AccessCode req2 = new AccessCode(code2);
-
-        AccessCode valid2 = new AccessCode("tempUser", code2, before1, after1);
-        //when(repository.findByData(code1)).thenReturn(valid1)
-
-        /* Permanent Access Code valid1 */
-        assertEquals(testee.matches(req1, valid1, "in"), valid1.getPersonID());
-        assertEquals(testee.matches(req1, valid1, "out"), valid1.getPersonID());
+        /* Permanent Access Code valid */
+        assertEquals("Permanent Access Code in", testee.matches(req1, valid, "in"), valid.getPersonID());
+        assertEquals("Permanent Access Code out", testee.matches(req1, valid, "out"), valid.getPersonID());
 
         /* Test if exception is thrown when code is invalid */
         try {
-            testee.matches(req2, valid1, "in");
-            fail();
+            testee.matches(req2, valid, "in");
+            fail("Wrong Code, in: Exception not thrown");
         } catch (ValidationException ignored) {}
         try {
-            testee.matches(req2, valid1, "out");
-            fail();
+            testee.matches(req2, valid, "out");
+            fail("Wrong Code, out: Exception not thrown");
         } catch (ValidationException ignored) {}
 
         /* Testing Temporary access code valid */
+        valid = new AccessCode("user", code1, before1, after1);
+        assertEquals("In: Temporary access code valid", testee.matches(req1, valid, "in"),  valid.getPersonID());
+        assertEquals("Out: Temporary access code valid", testee.matches(req1, valid, "out"), valid.getPersonID());
+
+        valid = new AccessCode("user", code1, before2, before1);
+        try {
+            testee.matches(req1, valid, "in");
+            fail("Code Entrance Expired: Exception not thrown");
+        } catch (ValidationException ignored) {}
+        assertEquals("Code", testee.matches(req1, valid, "out"), valid.getPersonID());
+
+        valid = new AccessCode("user", code1, after1, after2);
+        try {
+            testee.matches(req1, valid, "in");
+            fail("Future Code, in: Exception not thrown");
+        } catch (ValidationException ignored) {}
+        try {
+            testee.matches(req1, valid, "out");
+            fail("Future Code, out: Exception not thrown");
+        } catch (ValidationException ignored) {}
     }
 
     /**
