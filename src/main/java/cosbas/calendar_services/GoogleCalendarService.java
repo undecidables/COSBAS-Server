f7c25da3692cbf4490123fe7e52f57fc52b54ddf
Jason Richard Evans <evans.richard.jason@gmail.com>
2015-09-03 17:11:44 +0200

    Getting Service Object and Deleting an Event

--- src/main/java/cosbas/calendar_services/GoogleCalendarService.java
+++ src/main/java/cosbas/calendar_services/GoogleCalendarService.java
@@ -1,5 +1,6 @@
 package cosbas.calendar_services;
 
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -11,6 +12,7 @@ import com.google.api.services.calendar.model.EventAttendee;
 import com.google.api.services.calendar.model.EventDateTime;
 import com.google.api.services.calendar.model.EventReminder;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
@@ -20,11 +22,12 @@ import java.util.List;
  * @author Jason Richard Evans
  */
 public class GoogleCalendarService implements CalendarService {
-    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+    private static final String APPLICATION_NAME = "COSBAS Calendar Integration Service";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static HttpTransport HTTP_TRANSPORT;
     private static final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR);
     private final String CALENDAR_ID = "primary";
+    private com.google.api.services.calendar.Calendar service;
 
     static {
         try {
@@ -50,7 +53,7 @@ public class GoogleCalendarService implements CalendarService {
     public String makeAppointment(String emplid, LocalDateTime startTime, int Duration, String clientName, String clientEmail) {
         //TO DO get the service object
         Event event = new Event()
-                .setSummary(SUMMARY)
+                .setSummary(SUMMARY + " " + clientEmail)
                 .setDescription(clientName + " has an appointment with " + emplid + " at " + startTime.toString());
 
         DateTime startDateTime = toDateTime(startTime);
@@ -79,13 +82,27 @@ public class GoogleCalendarService implements CalendarService {
                 .setOverrides(Arrays.asList(reminderOverride));
         event.setReminders(reminders);
 
-        //event = service.events().insert(calendarId, event).execute();
-        //return event.getHtmlLink();
+        try{
+            service = getCalendarService();
+            event = service.events().insert(CALENDAR_ID, event).execute();
+            return event.getHtmlLink();
+        }
+        catch(IOException e){
+            System.out.println("COSBAS Calendar: In GoogleCalendarService could not initialize the service");
+        }
         return null;
     }
 
     @Override
     public boolean removeAppointment(String emplid, String clientEmail) {
+        try{
+            service = getCalendarService();
+            service.events().delete(CALENDAR_ID, SUMMARY + " " + clientEmail).execute();
+            return true;
+        }
+        catch(IOException e){
+            System.out.println("COSBAS Calendar: In GoogleCalendarService could not initialize the service");
+        }
         return false;
     }
 
@@ -99,5 +116,11 @@ public class GoogleCalendarService implements CalendarService {
         sDateTime += "+02:00";
         DateTime newTime = new DateTime(sDateTime);
         return newTime;
+    }
+
+    public static com.google.api.services.calendar.Calendar getCalendarService() throws IOException {
+        return new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, (Credential)credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 }
