7ce1bfd249cb78c7ec77990652381e100828704d
Renette Ros <renetteros@gmail.com>
2015-10-05 17:54:34 +0200

    Overrode hashcode and changed equals to use apache commons

--- src/main/java/cosbas/user/ContactDetail.java
+++ src/main/java/cosbas/user/ContactDetail.java
@@ -1,6 +1,10 @@
 package cosbas.user;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.springframework.data.annotation.PersistenceConstructor;
+
+
 
 /**
  * {@author Renette}
@@ -32,7 +36,19 @@ public class ContactDetail {
             return false;
         }
         final ContactDetail other = (ContactDetail) object;
-        return details.equals(other.details) && type.equals(other.type);
+        return new EqualsBuilder()
+                .append(details, other.details)
+                .append(type, other.type)
+                .isEquals();
+    }
+
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(79, 113)
+                .append(details)
+                .append(type)
+                .toHashCode();
     }
 }
 
