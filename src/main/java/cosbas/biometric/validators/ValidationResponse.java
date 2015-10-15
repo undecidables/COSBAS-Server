6bb8dcf944895a42306845ba3ab41c52225ce77e
Szymon Ziolkowski <nomyzs.z@gmail.com>
2015-10-15 11:18:42 +0200

    Added JAVACV + OPENCV  + many more dependencies and commented out logging as it is breaking again

--- src/main/java/cosbas/biometric/validators/ValidationResponse.java
+++ src/main/java/cosbas/biometric/validators/ValidationResponse.java
@@ -16,14 +16,14 @@ public class ValidationResponse {
     /* A number between 0 and 1 */
     public final double certainty;
 
-    ValidationResponse(boolean approved, String data, int certainty) {
+    public ValidationResponse(boolean approved, String data, double certainty) {
         this.data = data;
         this.approved = approved;
         this.certainty = certainty;
     }
 
-    ValidationResponse(boolean approved, String data, int certaintyPercent) {
-     this(approved, data, 1.0)
+    public ValidationResponse(boolean approved, String data) {
+     this(approved, data, 1.0);
     }
 
     public static ValidationResponse successfulValidation(String user) {
