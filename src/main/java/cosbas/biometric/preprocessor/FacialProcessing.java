07f126493aba5f1b5a66de1bd7b2c9e3c96de8a6
Renette Ros <renetteros@gmail.com>
2015-10-21 12:08:16 +0200

    Some more processing functions.

--- src/main/java/cosbas/biometric/preprocessor/FacialProcessing.java
+++ src/main/java/cosbas/biometric/preprocessor/FacialProcessing.java
@@ -4,23 +4,49 @@ import cosbas.biometric.BiometricTypes;
 import cosbas.biometric.data.BiometricData;
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_highgui;
+import org.bytedeco.javacpp.opencv_imgproc;
 import org.springframework.stereotype.Component;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.bytedeco.javacpp.opencv_core.CV_8UC1;
 import static org.bytedeco.javacpp.opencv_core.cvMat;
 import static org.bytedeco.javacpp.opencv_highgui.CV_LOAD_IMAGE_GRAYSCALE;
 import static org.bytedeco.javacpp.opencv_highgui.cvDecodeImage;
+import static org.bytedeco.javacpp.opencv_highgui.imdecode;
 import static org.bytedeco.javacpp.opencv_imgproc.cvResize;
+import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
 
 /**
  * {@author Renette}
  */
 @Component
 public class FacialProcessing implements BiometricsPreprocessor {
-    private int imageSize = 150;
+    private int scaledFaceSize = 150;
 
-    private opencv_core.IplImage createIPL(byte[] b) {
+    private opencv_core.IplImage createGrayscaleIpl(byte[] b) {
         return cvDecodeImage(cvMat(1, b.length, CV_8UC1, new BytePointer(b)), CV_LOAD_IMAGE_GRAYSCALE);
+    }
+
+    private BufferedImage byteArrayToImage(byte[] b) throws IOException {
+        InputStream in = new ByteArrayInputStream(b);
+        return ImageIO.read(in);
+    }
+
+    private opencv_core.Mat byteArrayToMat(byte[] b) throws IOException {
+        BufferedImage img = byteArrayToImage(b);
+
+        int rows = img.getWidth();
+        int cols = img.getHeight();
+        int type = opencv_core.CV_8UC1;
+        opencv_core.Mat mat = new opencv_core.Mat(rows, cols, type);
+        mat.data(new BytePointer(b));
+        return mat;
     }
 
     private byte[] iplToByteArray(opencv_core.IplImage image) {
@@ -35,7 +61,7 @@ public class FacialProcessing implements BiometricsPreprocessor {
 
     @Override
     public BiometricData processRegister(byte[] data, BiometricTypes type) {
-        opencv_core.IplImage iplImage = createIPL(data);
+        opencv_core.IplImage iplImage = createGrayscaleIpl(data);
 
         opencv_core.IplImage resizedImage = resizeImage(iplImage);
 
@@ -45,7 +71,7 @@ public class FacialProcessing implements BiometricsPreprocessor {
     }
 
     private opencv_core.IplImage resizeImage(opencv_core.IplImage original) {
-        opencv_core.IplImage resizedImage = opencv_core.IplImage.create(imageSize, imageSize, original.depth(), original.nChannels());
+        opencv_core.IplImage resizedImage = opencv_core.IplImage.create(scaledFaceSize, scaledFaceSize, original.depth(), original.nChannels());
         cvResize(original, resizedImage);
         return resizedImage;
     }
