7084334283f0fc1a735483d92400daad4b03d60f
Renette Ros <renetteros@gmail.com>
2015-10-21 20:36:09 +0200

    Fixed some stuff.

--- src/main/java/cosbas/biometric/preprocessor/FacialProcessing.java
+++ src/main/java/cosbas/biometric/preprocessor/FacialProcessing.java
@@ -4,19 +4,13 @@ import cosbas.biometric.BiometricTypes;
 import cosbas.biometric.data.BiometricData;
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.opencv_core;
-import org.bytedeco.javacpp.opencv_imgproc;
 import org.springframework.stereotype.Component;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
-import static org.bytedeco.javacpp.opencv_core.CV_8UC1;
+import static org.bytedeco.javacpp.opencv_core.CV_8UC3;
 import static org.bytedeco.javacpp.opencv_core.cvMat;
-import static org.bytedeco.javacpp.opencv_highgui.CV_LOAD_IMAGE_GRAYSCALE;
-import static org.bytedeco.javacpp.opencv_highgui.cvDecodeImage;
+import static org.bytedeco.javacpp.opencv_highgui.*;
 import static org.bytedeco.javacpp.opencv_imgproc.cvResize;
 import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
 
@@ -27,35 +21,34 @@ import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
 public class FacialProcessing implements BiometricsPreprocessor {
     private int scaledFaceSize = 150;
 
-    private opencv_core.IplImage createIPl(byte[] b) {
-        return cvDecodeImage(cvMat(1, b.length, CV_8UC1, new BytePointer(b)), CV_LOAD_IMAGE_GRAYSCALE);
+    opencv_core.IplImage grayscaleIpl(byte[] b) {
+        return cvDecodeImage(cvMat(1, b.length, CV_8UC3, new BytePointer(b)), CV_LOAD_IMAGE_GRAYSCALE);
     }
 
-    private BufferedImage byteArrayToImage(byte[] b) throws IOException {
-        InputStream in = new ByteArrayInputStream(b);
-        return ImageIO.read(in);
+    opencv_core.Mat byteArrayToMat(byte[] b) throws IOException {
+        return new opencv_core.Mat(grayscaleIpl(b));
     }
 
-    private opencv_core.Mat byteArrayToMat(byte[] b) throws IOException {
-        BufferedImage img = byteArrayToImage(b);
+    byte[] matToByteAray(opencv_core.Mat mat) {
+        byte[] arr = new byte[mat.rows() * mat.cols()];
+        boolean res = imencode(".png", mat, arr);
 
-        int rows = img.getWidth();
-        int cols = img.getHeight();
-        int type = opencv_core.CV_8UC3;
-        return new opencv_core.Mat(rows, cols, type, new BytePointer(b));
+        return arr;
     }
 
-    private opencv_core.Mat grayscaleMat(opencv_core.Mat m) {
-        opencv_core.Mat newMat = new opencv_core.Mat(m.rows(), m.cols(), CV_8UC1);
-        cvtColor(m, newMat, opencv_imgproc.COLOR_BGR2GRAY);
 
-        return newMat;
+    byte[] iplToByteArray(opencv_core.IplImage image) {
+        return matToByteAray(new opencv_core.Mat(image));
     }
 
-    private byte[] iplToByteArray(opencv_core.IplImage image) {
-        opencv_core.Mat mat = new opencv_core.Mat(image);
-        return mat.data().asBuffer().array();
+    private opencv_core.IplImage resizeImage(opencv_core.IplImage original) {
+        opencv_core.IplImage resizedImage = opencv_core.IplImage.create(scaledFaceSize, scaledFaceSize, original.depth(), original.nChannels());
+        cvResize(original, resizedImage);
+        return resizedImage;
     }
+
+
+
 
     @Override
     public BiometricData processAccess(byte[] data, BiometricTypes type) {
@@ -64,7 +57,7 @@ public class FacialProcessing implements BiometricsPreprocessor {
 
     @Override
     public BiometricData processRegister(byte[] data, BiometricTypes type) {
-        opencv_core.IplImage iplImage = createIPl(data);
+        opencv_core.IplImage iplImage = grayscaleIpl(data);
 
         opencv_core.IplImage resizedImage = resizeImage(iplImage);
 
@@ -73,9 +66,5 @@ public class FacialProcessing implements BiometricsPreprocessor {
         return new BiometricData(type, newData);
     }
 
-    private opencv_core.IplImage resizeImage(opencv_core.IplImage original) {
-        opencv_core.IplImage resizedImage = opencv_core.IplImage.create(scaledFaceSize, scaledFaceSize, original.depth(), original.nChannels());
-        cvResize(original, resizedImage);
-        return resizedImage;
-    }
+
 }
