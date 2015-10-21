dfa1f9940ed94cb81545fe059d69ed1dd24de53c
Szymon Ziolkowski <nomyzs.z@gmail.com>
2015-10-21 14:29:49 +0200

    Changed the reports.html and added linking

--- src/main/java/cosbas/web/ReportsController.java
+++ src/main/java/cosbas/web/ReportsController.java
@@ -1,6 +1,8 @@
 package cosbas.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *  {@author Szymon}
@@ -8,4 +10,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ReportsController {
+
+    @RequestMapping(value = "/reports", method = RequestMethod.GET)
+    public String status()
+    {
+        return "reports";
+    }
 }
