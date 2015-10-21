43436829f73671b2fbe968807a43c8009c49a770
Renette Ros <renetteros@gmail.com>
2015-10-21 14:42:21 +0200

    Started experimenting with some caching stuff

--- src/main/java/cosbas/permissions/PermissionsDAO.java
+++ src/main/java/cosbas/permissions/PermissionsDAO.java
@@ -1,5 +1,6 @@
 package cosbas.permissions;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -7,9 +8,13 @@ import java.util.List;
 /**
  *  {@author Renette}
  */
+
 public interface PermissionsDAO extends CrudRepository<Permission, String>{
+    @Cacheable("Permissions-UserID")
     List<Permission> findByUserID(String userID);
+    @Cacheable("Permissions-PermissionID")
     List<Permission> findByPermission(PermissionId permission);
+    @Cacheable("Permissions-Both")
     Permission findByUserIDAndPermission(String userID, PermissionId permission);
     List<Permission> deleteByUserIDAndPermission(String userID, PermissionId permission);
 }
--- src/main/java/cosbas/web/Application.java
+++ src/main/java/cosbas/web/Application.java
@@ -2,6 +2,7 @@ package cosbas.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ImportResource("beans.xml")
 @ComponentScan("cosbas")
 @EnableScheduling
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) throws Throwable {
