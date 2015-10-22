8649a2e484ca24af3f0dbe4968e1061fce963410
TieniePritchard <u12056741@tuks.co.za>
2015-10-22 16:12:47 +0200

    Fix to Nav Link Hovering

--- src/main/resources/static/resources/scripts/jquery.js
+++ src/main/resources/static/resources/scripts/jquery.js
@@ -1,4 +1,5 @@
 $(document).ready(function(e) {
     $("body").css("visibility", "visible");
+    e.stopImmediatePropagation();
      $('.nav li').removeClass('active');//or $('.active').removeClass('active');
 });
