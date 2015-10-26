09ab6b9d96e222b4b6f56851f285fca2317febf2
Szymon Ziolkowski <nomyzs.z@gmail.com>
2015-10-26 22:26:00 +0200

    Forgot about UserDAO

--- src/main/java/cosbas/user/UserDAO.java
+++ src/main/java/cosbas/user/UserDAO.java
@@ -1,10 +1,32 @@
 package cosbas.user;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * @author Renette
  */
-public interface UserDAO extends CrudRepository<User, String> {
+public interface UserDAO {
+
+    @CacheEvict(value = "users",beforeInvocation = true, allEntries = true)
+    <S extends User> S save(S entity);
+    @CacheEvict(value = "users",beforeInvocation = true, allEntries = true)
+    <S extends User> Iterable<S> save(Iterable<S> entities);
+    @CacheEvict(value = "users",beforeInvocation = true, allEntries = true)
+    void delete(User entity);
+    @CacheEvict(value = "users",beforeInvocation = true, allEntries = true)
+    void delete(Iterable<? extends User> entities);
+    @CacheEvict(value = "users",beforeInvocation = true, allEntries = true)
+    void deleteAll();
+    @CacheEvict(value = "users",beforeInvocation = true, allEntries = true)
+    void delete(String userID);
+
+    long count();
+    @Cacheable("users")
+    Iterable<User> findAll();
+    @Cacheable("users")
     User findByUserID(String userID);
+    @Cacheable("users")
+    User findOne(String userID);
 }
