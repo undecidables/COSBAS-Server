d4fe5cedcd8e2c4a80ecf8e5231daf0bb099971a
Renette Ros <renetteros@gmail.com>
2015-10-27 15:49:51 +0200

    Fixed buffer overflow I hope.

--- src/main/java/cosbas/biometric/validators/facial/CvMatStorage.java
+++ src/main/java/cosbas/biometric/validators/facial/CvMatStorage.java
@@ -1,5 +1,6 @@
 package cosbas.biometric.validators.facial;
 
+import org.bytedeco.javacpp.DoublePointer;
 import org.bytedeco.javacpp.opencv_core;
 import org.springframework.data.annotation.PersistenceConstructor;
 
@@ -11,13 +12,14 @@ public class CvMatStorage {
     private final int cols;
     private final int rows;
     private final int type;
-    private double[] t;
+    private double[] data;
 
     @PersistenceConstructor
-    public CvMatStorage(int cols, int rows, int type) {
+    public CvMatStorage(int cols, int rows, int type, double[] data) {
         this.cols = cols;
         this.rows = rows;
         this.type = type;
+        this.data = data;
     }
 
     CvMatStorage(opencv_core.IplImage image) {
@@ -28,11 +30,14 @@ public class CvMatStorage {
         cols = image.cols();
         rows = image.rows();
         type = image.type();
-        t = image.get();
-    }
+        data = image.get();
+
+}
     public opencv_core.CvMat getCVMat() {
         opencv_core.CvMat mat = opencv_core.CvMat.create(rows, cols, type);
-        mat.put(t);
+        mat.height(rows);
+        mat.width(cols);
+        mat.data_db(new DoublePointer(data));
         return mat;
     }
 
