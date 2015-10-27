3bc4eccca3e6d80f75e84660810f1288d55da048
Szymon Ziolkowski <nomyzs.z@gmail.com>
2015-10-27 10:24:41 +0200

    Added cacheing annotations to calendarwrapper but for some reason it doesnt want to cache

--- src/main/java/cosbas/calendar_services/authorization/CalendarDBAdapter.java
+++ src/main/java/cosbas/calendar_services/authorization/CalendarDBAdapter.java
@@ -1,12 +1,30 @@
 package cosbas.calendar_services.authorization;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 /**
  * @author Jason Richard Evans
  */
-public interface CalendarDBAdapter extends CrudRepository<CredentialWrapper, String> {
+public interface CalendarDBAdapter {
+
+    @CacheEvict(value = "credentialWrapper",beforeInvocation = true, allEntries = true)
+    <S extends CredentialWrapper> S save(S entity);
+    @CacheEvict(value = "credentialWrapper",beforeInvocation = true, allEntries = true)
+    <S extends CredentialWrapper> Iterable<S> save(Iterable<S> entities);
+    @CacheEvict(value = "credentialWrapper",beforeInvocation = true, allEntries = true)
+    void delete(CredentialWrapper entity);
+    @CacheEvict(value = "credentialWrapper",beforeInvocation = true, allEntries = true)
+    void delete(Iterable<? extends CredentialWrapper> entities);
+    @CacheEvict(value = "credentialWrapper",beforeInvocation = true, allEntries = true)
+    void deleteAll();
+
+    long count();
+
+    @Cacheable("credentialWrapper")
     CredentialWrapper findByStaffID(String staffID);
+    @Cacheable("credentialWrapper")
     List<CredentialWrapper> findAll();
 }
