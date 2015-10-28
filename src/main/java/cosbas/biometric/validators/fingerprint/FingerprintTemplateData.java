dd6c5fbe2756ea2224b054eb0a1a7fa1d47977d8
Vivian <u13238435@tuks.co.za>
2015-10-28 16:38:43 +0200

    Wrote a small unit test for fingerprint matching

--- src/main/java/cosbas/biometric/validators/fingerprint/FingerprintTemplateData.java
+++ src/main/java/cosbas/biometric/validators/fingerprint/FingerprintTemplateData.java
@@ -1,21 +1,13 @@
 package cosbas.biometric.validators.fingerprint;
 
-import com.google.api.client.util.Value;
 import cosbas.biometric.BiometricTypes;
 import cosbas.biometric.data.BiometricData;
-import cosbas.biometric.data.BiometricDataDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.PersistenceConstructor;
-import org.springframework.data.annotation.Transient;
-
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.ArrayList;
 
 /**
- *  {@Author Vivian Venter}
+ *  {@author Vivian Venter}
  *  The class to save the template data of each fingerprint
  */
 public class FingerprintTemplateData extends BiometricData{
--- src/test/java/cosbas/biometric/validators/fingerprint/FingerprintMatchingTest.java
+++ src/test/java/cosbas/biometric/validators/fingerprint/FingerprintMatchingTest.java
@@ -2,12 +2,22 @@ package cosbas.biometric.validators.fingerprint;
 
 import cosbas.biometric.data.BiometricData;
 import cosbas.biometric.data.BiometricDataDAO;
+import cosbas.biometric.request.DoorActions;
+import cosbas.biometric.validators.ValidationResponse;
+import org.apache.commons.lang.StringUtils;
+import org.aspectj.lang.annotation.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 /**
@@ -16,24 +26,59 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class FingerprintMatchingTest {
 
-    private final String folder = "./src/test/resources/fingerprints";
+    private final String path = "./src/test/resources/fingerprints/";
+    private final String finger1 = "test100.bmp";
+
+    private final String[] fingers = {"test300.bmp","test400.bmp"};
 
     FingerprintMatching testee;
+    FingerprintTemplateCreator creator;
+    BufferedImage originalImage;
     BiometricDataDAO biometricRepo;
-    private List<BiometricData> trainingList = new LinkedList<>();
+    private List<BiometricData> fingersList = new LinkedList<>();
 
-    /*@Before
-    public void start() {
+    @org.junit.Before
+    public void start() throws IOException {
         biometricRepo = mock(BiometricDataDAO.class);
-        when(biometricRepo.findByUserID("BCrawley")).thenReturn(trainingList);
-    }*/
+        when(biometricRepo.findByUserID("BCrawley")).thenReturn(fingersList);
+
+        originalImage = ImageIO.read(new File(path+finger1));
+        byte[] dbItem = imagetobyte(originalImage);
+        testee = new FingerprintMatching(dbItem,"BCrawley");
+
+
+    }
+
+    private byte[] imagetobyte(BufferedImage originalImage) throws IOException {
+        ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+        ImageIO.write(originalImage,"bmp",byteArray);
+        byteArray.flush();
+        byte[] dbItem = byteArray.toByteArray();
+        byteArray.close();
+
+        return dbItem;
+    }
 
 
 
     @Test
-    public void testFingerprintMatching() {
+    public void testFingerprintMatching() throws IOException {
+        creator = new FingerprintTemplateCreator();
 
+        BufferedImage newImage;
+        for (int i = 0; i < fingers.length; i++) {
+            newImage = ImageIO.read(new File(path+fingers[i]));
 
+            FingerprintTemplateData tester = creator.createTemplateFingerprintData("BCrawley",newImage,false);
+            ValidationResponse response = testee.matches(tester,"BCrawley", DoorActions.IN);
+
+            if (response.approved) {
+                System.out.println("Returned Certainty: " + response.certainty);
+                return;
+            } else {
+                System.out.println("Returned Certainty too low try again: " + response.certainty);
+            }
+        }
     }
 
 }
