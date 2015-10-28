7e94f3134c71356e3ed9d7baa76440dff14f9cd7
Renette Ros <renetteros@gmail.com>
2015-10-28 11:27:19 +0200

    Changed BiometricDAO according to Notifcations branch

--- src/main/java/cosbas/biometric/data/BiometricDataDAO.java
+++ src/main/java/cosbas/biometric/data/BiometricDataDAO.java
@@ -19,25 +19,33 @@ public interface BiometricDataDAO {
      * Apply @CacheEvict annotation on functions which will alter the tables/collections in the database, apply @Cacheable annotations to functions that do not alter the database in any way.
      */
 
-    @CacheEvict(value = "biometricDatas", beforeInvocation = true, allEntries = true)
+    @CacheEvict(value = "Biometric-Data",beforeInvocation = true, allEntries = true)
     <S extends BiometricData> S save(S entity);
-    @CacheEvict(value = "biometricDatas",beforeInvocation = true, allEntries = true)
+    @CacheEvict(value = "Biometric-Data",beforeInvocation = true, allEntries = true)
     <S extends BiometricData> Iterable<S> save(Iterable<S> entities);
-    @CacheEvict(value = "biometricDatas",beforeInvocation = true, allEntries = true)
+    @CacheEvict(value = "Biometric-Data",beforeInvocation = true, allEntries = true)
     void delete(BiometricData entity);
-    @CacheEvict(value = "biometricDatas",beforeInvocation = true, allEntries = true)
+    @CacheEvict(value = "Biometric-Data",beforeInvocation = true, allEntries = true)
     void delete(Iterable<? extends BiometricData> entities);
-    @CacheEvict(value = "biometricDatas",beforeInvocation = true, allEntries = true)
+    @CacheEvict(value = "Biometric-Data",beforeInvocation = true, allEntries = true)
     void deleteAll();
-    @CacheEvict(value = "biometricDatas",beforeInvocation = true, allEntries = true)
+    @CacheEvict(value = "Biometric-Data",beforeInvocation = true, allEntries = true)
     List<BiometricData> deleteByUserID(String userID);
 
-    @Cacheable("biometricDatas")
+    @Cacheable("Biometric-Data")
     Iterable<BiometricData> findAll();
-    @Cacheable("biometricDatas")
+    @Cacheable("Biometric-Data")
     List<BiometricData> findByUserID(String userID);
-    @Cacheable("biometricDatas")
+    @Cacheable("Biometric-Data")
     List<BiometricData> findByType (BiometricTypes type);
-    @Cacheable("biometricDatas")
+    @Cacheable("Biometric-Data")
     BiometricData findByData (byte[] data);
+    
+
+    @Cacheable("Biometric-Data")
+    List<TemporaryAccessCode> findByAppointmentID(String appointmentID);
+
+    @Cacheable("Biometric-Data")
+    TemporaryAccessCode findById(String id);
+
 }
