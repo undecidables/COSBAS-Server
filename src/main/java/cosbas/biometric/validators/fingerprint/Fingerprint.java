239dfeece4a6314e1ba3acb4805a0717ce246b47
Vivian <u13238435@tuks.co.za>
2015-10-30 07:40:14 +0200

    Gave recognition to original writers

--- src/main/java/cosbas/biometric/validators/fingerprint/Fingerprint.java
+++ src/main/java/cosbas/biometric/validators/fingerprint/Fingerprint.java
@@ -5,7 +5,19 @@ import java.awt.image.BufferedImage;
 import java.util.*;
 
 /**
- * Give Recognition to writers
+ *  Developed by:   Thomas DEVAUX & Estelle SENAY
+                    (2007) Concordia University
+
+    This program is free software. You can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation. Either version 2.1 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+    more details.
+
  */
 public class Fingerprint {
 
