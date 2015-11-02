c4853a66dff47b174d2be94fe037e4b9e8676a0d
Renette Ros <renetteros@gmail.com>
2015-11-02 10:11:29 +0200

    Width/height injection fixed,

--- src/main/java/cosbas/biometric/preprocessor/FacialProcessing.java
+++ src/main/java/cosbas/biometric/preprocessor/FacialProcessing.java
@@ -1,5 +1,4 @@
 package cosbas.biometric.preprocessor;
-import com.google.api.client.util.Value;
 import cosbas.biometric.BiometricTypes;
 import cosbas.biometric.data.BiometricData;
 import cosbas.biometric.helper.FaceDetector;
@@ -7,6 +6,7 @@ import cosbas.biometric.helper.ImageProcessor;
 import cosbas.biometric.validators.exceptions.ValidationException;
 import org.bytedeco.javacpp.opencv_core;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -17,17 +17,35 @@ import org.springframework.stereotype.Component;
 public class FacialProcessing implements BiometricsPreprocessor {
 
     private final String encoding = ".png";
+
+    public void setImageHeight(int imageHeight) {
+        this.imageHeight = imageHeight;
+    }
+
+    @Autowired
+    public void setHelper(ImageProcessor<opencv_core.Mat> helper) {
+        this.helper = helper;
+    }
+
+    @Autowired
+    public void setDetector(FaceDetector detector) {
+        this.detector = detector;
+    }
+
+    public void setImageWidth(int imageWidth) {
+        this.imageWidth = imageWidth;
+    }
+
     @Value("${faces.imageWidth}")
-    private int imageWidth;
+    protected int imageWidth;
 
     @Value("${faces.imageHeight}")
-    private int imageHeight;
+    protected int imageHeight;
     @Autowired
     private ImageProcessor<opencv_core.Mat> helper;
 
     @Autowired
     private FaceDetector detector;
-
 
     /**
      * Gets the most center face from the image, scaled to the correct dimesions
